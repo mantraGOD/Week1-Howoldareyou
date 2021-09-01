f5e8faa72651af196b1c8dbe05ff97d5aa1a1828
Opilane TTHK <opilane@tthk.ee>
2021-09-01 12:11:39 +0300

    First Commit

---
+++ Fullname/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fullname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 1.programm küsib kasutaja käest kasutaja eesnime 
+            //Step 2.programm küsib kasutaja käest kasutaja perekonnanime
+            //Step 3.programm tervitab kasutajat kasutades neid andmeid
+            Console.WriteLine("Hello,What is you name?");
+            string username = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {username}");
+
+            Console.WriteLine("What is your lastname?");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine($"You have a good lastname, {lastname}");
+
+            Console.WriteLine($"Nice to meet you,{username} {lastname}");
+
+            Console.WriteLine($"{username}, are you gay by any chance??");
+            Console.ReadLine();
+            Console.WriteLine($"WOW,{username}");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ howoldareyou/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace howoldareyou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            
+            String UserName = Console.ReadLine();
+            //Console.WriteLine("Hello, " + UserName + "!");
+            //String interpolation
+            Console.WriteLine ($"Hello,{UserName} ");
+           
+            Console.WriteLine("how old are you? Enter digits only:");
+
+            int Age = Int32.Parse(Console.ReadLine());
+            
+            int YearOfBirth = 2021 - Age;
+            
+            Console.WriteLine($"You were born in {YearOfBirth}");
+
+            
+            
+            Console.Read();
+
+
+        }   
+
+    }
+}
